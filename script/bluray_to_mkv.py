73625a11aa13b90fa248347200c0fe129c31735e
Alexandre Figura <git@alexandre.figura.live>
2015-04-06 15:20:34 +0200

    Refactoring of tests and source code.

--- script/bluray_to_mkv.py
+++ script/bluray_to_mkv.py
@@ -17,6 +17,7 @@
 ### NZBGET POST-PROCESSING SCRIPT                           ###
 ###############################################################
 
+from datetime import timedelta
 import logging
 import os
 import pathlib
@@ -31,10 +32,13 @@ ENVAR_MAKEMKV_PROFILE = "NZBPO_PROFILE"  # Path of MakeMKV XML profile
 ENVAR_MKV_DIRECTORY = "NZBPO_DIRECTORY"  # Directory path of converted movies
 ENVAR_MOVIE_TITLES = "NZBPP_TITLES"
 MAKEMKV_BINARY = "makemkvcon"
-MAKEMKV_PATTERN_TITLE_INFO = 'TINFO:(?P<number>\d+),\d+,\d+,'
-MAKEMKV_PATTERN_TITLE_FILE = '{}"(?P<fname>.+\.mkv)"'.format(MAKEMKV_PATTERN_TITLE_INFO)
-MAKEMKV_PATTERN_TITLE_DETAILS = '{}"(?P<name>.+) - (?P<chapters>\d+) chapter\(s\) , ' \
-                                '(?P<size>\d+\.?\d*) GB"'.format(MAKEMKV_PATTERN_TITLE_INFO)
+MAKEMKV_OUTPUT_CONVERSION_SUCCESS = 'Copy complete. 1 titles saved.'
+MAKEMKV_PATTERN_TITLE = 'TINFO:(?P<title>\d+),{id},\d+,"(?P<info>{pattern})"'
+MAKEMKV_PATTERN_TITLE_CHAPTERS = MAKEMKV_PATTERN_TITLE.format(id=8, pattern='(?P<chapters>\d+)')
+MAKEMKV_PATTERN_TITLE_DURATION = MAKEMKV_PATTERN_TITLE.format(
+    id=9, pattern='(?P<duration>(?P<hours>\d):(?P<minutes>\d{2}):(?P<seconds>\d{2}))')
+MAKEMKV_PATTERN_TITLE_SIZE = MAKEMKV_PATTERN_TITLE.format(id=10, pattern='(?P<size>\d{1,2}.\d+ GB)')
+MAKEMKV_PATTERN_TITLE_MKV = MAKEMKV_PATTERN_TITLE.format(id=27, pattern='(?P<mkv>.*\.mkv)')
 MSG_END_CONVERSION = "Successfully converted {} to MKV."
 NZBGET_LOG_FORMAT = "[%(levelname)s] %(message)s"
 POSTPROCESS_EXIT_CODE_ERROR = 94
@@ -42,140 +46,203 @@ POSTPROCESS_EXIT_CODE_SUCCESS = 93  # Returned code when post-process is success
 
 REQUIRED_OPTIONS = (ENVAR_MAKEMKV_PROFILE, ENVAR_MKV_DIRECTORY)
 
-
+# Logging configuration.
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
+logging.addLevelName(logging.DEBUG, "DETAIL")  # NZBGet prefix for DEBUG messages
 
 
 def is_configured():
-    missing_opt = False
+    """Verify that script configuration options are defined.
+
+    :return: ``True`` if all options are defined; ``False`` otherwise.
+    """
+    missing_option = False
     for option in REQUIRED_OPTIONS:
         if option not in os.environ:
-            logger.error("The following configuration option must be defined: {}.".format(option))
-            missing_opt = True
-    if missing_opt:
+            logger.error("Unable to start Blu-Ray to MKV conversion. "
+                         "The following configuration option must be defined: {}.".format(option))
+            missing_option = True
+    if missing_option:
         return False
     return True
 
 
 def find_makemkv_binary():
+    """Search for the binary of MakeMKV command line tool.
+
+    :return: a ``PurePath`` object pointing to the MakeMKV binary, if this latter is found; ``None`` otherwise.
+    """
     try:
-        bin_path = subprocess.check_output(
+        binary_path = subprocess.check_output(
             ['which', MAKEMKV_BINARY], stderr=subprocess.DEVNULL, universal_newlines=True)
     except subprocess.CalledProcessError:
         logger.error("MakeMKV binary not found.")
         return None
-    return pathlib.PurePath(bin_path.rstrip())
+    return pathlib.PurePath(binary_path.rstrip())
 
 
-def find_blu_ray_sources(path, multi=1):
-    sources_type = None
-    sources = list(path.rglob('BDMV/index.bdmv')) or None
+def find_blu_ray_discs(path, disc_set=1):
+    """Search for Blu-Ray discs in a directory.
 
-    if sources:
-        sources_type = "file"
-        for i in range(len(sources)):
-            sources[i] = sources[i].parents[1]
+    Blu-Ray discs can be stored either as subdirectories or iso images.
+
+    :param path: directory where to perform the search.
+    :param disc_set: number of discs to return among all the discs found.
+
+    :return: a tuple in the form (disc types, discs found). The disc type is either "file" or "iso".
+             The discs found are a list of ``Path`` objects and are sorted by name for "file" type,
+             or in descending order of size for "iso" type.
+    """
+    disc_type = None
+    if disc_set <= 0:
+        return disc_type, list()
+
+    path = pathlib.Path(path)
+    discs = sorted(path.rglob('BDMV/index.bdmv'))
+    if discs:
+        disc_type = "file"
+        for i in range(len(discs)):
+            discs[i] = discs[i].parents[1]  # Keep root directory of the disc
     else:
         iso_images = path.rglob('*.iso')
-        sources = sorted(iso_images, key=lambda iso: iso.stat().st_size, reverse=True) or None
-        if sources:
-            sources_type = "iso"
+        # Bigger discs are given more importance.
+        discs = sorted(iso_images, key=lambda iso: iso.stat().st_size, reverse=True)
+        if discs:
+            disc_type = "iso"
 
-    if sources:
-        sources_number = len(sources)
-        if multi == 1:
-            if sources_number > 1:
-                logger.warning("More than one blu-ray source was found.")
-            sources = sources[0]
-        elif multi > 1:
-            if sources_number != multi:
-                logger.warning("{0} blu-ray sources were found ({1} asked).".format(sources_number, multi))
-            sources = sources[:multi]
+    if discs:
+        logger.debug("{0} discs were found in {1}".format(len(discs), path))
+        discs = discs[:disc_set]
 
-    return sources_type, sources
+    return disc_type, discs
 
 
-def identify_movie_titles(source, multi=1):
-    makemkv = find_makemkv_binary()
-    title_fname = re.compile(MAKEMKV_PATTERN_TITLE_FILE)
-    title_details = re.compile(MAKEMKV_PATTERN_TITLE_DETAILS)
+def identify_movie_titles(disc, count=1):
+    """Browse through a Blu-Ray disc and look at the movie titles.
+
+    As a disc can contain several movie titles and additional features, this function does its utmost
+    to return the most relevant titles. If there is any doubt, no title is returned.
+
+    :param disc: path of Blu-Ray disc to analyze.
+    :param count: number of titles to extract from the disc.
+
+    :return: a ``list`` of titles, sorted in descending order of duration. Each title is a ``dictionary`` with:
+             - a number of ``chapters``,
+             - a ``duration`` (``timedelta`` object),
+             - a ``size`` in GB,
+             - a ``mkv`` file name (used by MakeMKV during the conversion process).
+    """
     titles = list()
+    if count <= 0:
+        return titles
 
+    # List of regular expressions to match for retrieving titles information.
+    # Each regex is associated to a conversion function.
+    regexs = sorted([
+        {
+            'name': 'chapters',
+            'pattern': re.compile(MAKEMKV_PATTERN_TITLE_CHAPTERS),
+            'converter': lambda m: int(m.group('chapters')),
+        },
+        {
+            'name': 'duration',
+            'pattern': re.compile(MAKEMKV_PATTERN_TITLE_DURATION),
+            'converter': lambda m: timedelta(**{unit: int(m.group(unit)) for unit in ('hours', 'minutes', 'seconds')}),
+        },
+        {
+            'name': 'size',
+            'pattern': re.compile(MAKEMKV_PATTERN_TITLE_SIZE),
+            'converter': lambda m: float(m.group('size').split()[0]),
+        },
+        {
+            'name': 'mkv',
+            'pattern': re.compile(MAKEMKV_PATTERN_TITLE_MKV),
+            'converter': lambda m: m.group('mkv'),
+        },
+    ], key=lambda regex: int(regex['pattern'].pattern.split(',')[1]))  # Sorting on MakeMKV INFO ID
+
+    makemkv = [find_makemkv_binary(), '-r', 'info', '{type}:"{path}"'.format(**disc)]
     with subprocess.Popen(
-            [makemkv, '-r', 'info', '{type}:{path}'.format(**source)],
-            stderr=subprocess.STDOUT,  stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as p:
+            makemkv, stderr=subprocess.STDOUT,  stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as p:
+        title = dict()
         line = p.stdout.readline()
         while line:
-            line = line.rstrip()
-            m = title_fname.match(line)
-            if m is not None:
-                fname = m.group('fname')
-                line = p.stdout.readline()
+            for regex in regexs:
                 while line:
                     line = line.rstrip()
-                    m = title_details.match(line)
-                    if m is not None:
-                        number = int(m.group('number'))
-                        chapters = int(m.group('chapters'))
-                        size = float(m.group('size'))
-                        titles.append({'number': number, 'fname': fname, 'chapters': chapters, 'size': size})
+                    match = regex['pattern'].match(line)
+                    if match is not None:
+                        title[regex['name']] = regex['converter'](match)
+                        if regex['name'] == regexs[-1]['name']:
+                            title['number'] = int(match.group('title'))
+                            titles.append(title)
+                            title = dict()
                         break
                     line = p.stdout.readline()
             line = p.stdout.readline()
 
-    if not titles:
-        return None
-
-    titles = sorted(titles, key=lambda title: title['chapters'], reverse=True)
-    if multi == 1:
-        if len(titles) > 1:
-            if titles[0]['chapters'] == titles[1]['chapters']:
-                logger.warning("Two movie titles with the same number of chapters were found.")
-                return None
-        return titles[0]
-    elif multi > 1:
-        titles_number = len(titles)
-        if multi > titles_number:
-            logger.warning("Only {0} titles are available ({1} asked).".format(titles_number, multi))
-        return titles[:multi]
+    # Longer titles are given more importance.
+    titles = sorted(titles, key=lambda title: title['duration'], reverse=True)
+    for i in range(1, len(titles)):
+        # Sometimes, several movie titles co-exist, with same duration/number of chapters but different audio tracks.
+        if (titles[i]['duration'] == titles[i-1]['duration']) and (titles[i]['chapters'] == titles[i-1]['chapters']):
+            logger.warning("Identical movie titles were found for {}".format(disc['path']))
+            titles = list()
+            break
+    else:
+        titles = titles[:count]
 
     return titles
 
 
-def convert_to_mkv(movie, source, title, destination, profile):
-    makemkv = find_makemkv_binary()
+def convert_movie_to_mkv(movie, disc, title, destination_directory, makemkv_profile):
+    """Extract a movie title from a Blu-Ray disc and convert it into a MKV file.
 
-    p = subprocess.Popen(
-        [makemkv, '--profile={}'.format(profile), 'mkv', '{type}:{path}'.format(**source), title['number'], destination],
-        stderr=subprocess.STDOUT, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
-    line = p.stdout.readline()
-    while line:
-        line = line.rstrip()
-        logger.debug(line)
-        line = p.stdout.readline()
-    p.wait()
+    :param movie: name of the movie.
+    :param disc: Blu-Ray disc containing the movie. A ``dictionary`` with the disc ``type`` and ``path``.
+    :param title: movie title to convert. A ``dictionary`` with the ``number`` and ``mkv`` file name that MakeMKV uses
+                  internally to identifies the title.
+    :param destination_directory: path of the directory where the MKV file will be saved.
+    :param makemkv_profile: a MakeMKV profile to use for the conversion.
 
-    mkv_path = destination / title['fname']
-    if p.returncode != 0:
-        logger.error("An error was encountered during the conversion. Please check logs.")
+    :return: path of the newly created MKV file.
+    """
+    makemkv = [find_makemkv_binary(), '--profile="{}"'.format(makemkv_profile), 'mkv', '{type}:"{path}"'.format(**disc),
+               title['number'], destination_directory]
+
+    conversion_status = None
+    with subprocess.Popen(
+            makemkv, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as p:
+        line = last_line = p.stdout.readline()
+        while line:
+            line = line.rstrip()
+            last_line = line
+            logger.debug(line)
+            line = p.stdout.readline()
+        else:
+            # Even when MakeMKV encounters an error, its status code is often set to 0.
+            # That's why we use the last output line to check the conversion status.
+            conversion_status = last_line
+
+    mkv = destination_directory / title['mkv']
+    if conversion_status != MAKEMKV_OUTPUT_CONVERSION_SUCCESS:
         try:
-            mkv_path.unlink()
+            mkv.unlink()
         except OSError:
             pass
+        logger.error("Unable to convert the title {0} from disc {1}".format(title['number'], disc['path']))
         return None
 
-    mkv_new_path = mkv_path.with_name('{}.mkv'.format(movie))
+    mkv_target = mkv.with_name('{0} - {1}.mkv'.format(movie, title['number']))
     try:
-        mkv_path.rename(mkv_new_path)
+        mkv.rename(mkv_target)
     except OSError:
-        if not mkv_path.is_file():
-            logger.error("An error was encountered during the conversion. Please check logs.")
-        else:
-            logger.warning("Unable to rename {} to {}".format(mkv_path, mkv_new_path))
-        return None
+        logger.warning("Unable to rename {}".format(mkv))
+    else:
+        mkv = mkv_target
 
-    return mkv_new_path
+    return mkv
 
 
 if __name__ == '__main__':
--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,6 +1,7 @@
+from datetime import timedelta
 import os
 import sys
-import pathlib
+from pathlib import Path, PurePath
 import subprocess
 import unittest
 import unittest.mock
@@ -8,101 +9,118 @@ import unittest.mock
 sys.path.append(os.path.abspath('.'))
 from script import bluray_to_mkv as script
 
-FIXTURES_DIR = pathlib.Path('tests/fixtures')
+FIXTURES_DIR = Path('tests/fixtures')
 
 
 class ScriptTest(unittest.TestCase):
     @unittest.mock.patch('script.bluray_to_mkv.subprocess.check_output')
-    def test_find_makemkv_binary(self, mock_subprocess_output):
-        binary_path = pathlib.PurePath('/usr/bin', script.MAKEMKV_BINARY)
-        mock_subprocess_output.return_value = '{}\n'.format(binary_path)
+    def test_find_makemkvcon_binary(self, mock_subprocess_output):
+        binary_path = PurePath('/usr/bin/makemkvcon')
+        mock_subprocess_output.return_value = "{}\n".format(binary_path)
 
         self.assertEqual(script.find_makemkv_binary(), binary_path)
-        mock_subprocess_output.assert_called_once_with(
-            ['which', script.MAKEMKV_BINARY], stderr=subprocess.DEVNULL, universal_newlines=True)
 
     @unittest.mock.patch('script.bluray_to_mkv.subprocess.check_output')
-    def test_makemkv_must_be_installed(self, mock_subprocess_output):
-        command_line = ['which', script.MAKEMKV_BINARY]
-        mock_subprocess_output.side_effect = subprocess.CalledProcessError(returncode=1, cmd=command_line)
+    def test_unable_to_find_makemkvcon_binary(self, mock_subprocess_output):
+        mock_subprocess_output.side_effect = subprocess.CalledProcessError(returncode=1, cmd='which makemkvcon')
 
         self.assertEqual(script.find_makemkv_binary(), None)
-        mock_subprocess_output.assert_called_once_with(
-            command_line, stderr=subprocess.DEVNULL, universal_newlines=True)
 
-    def test_script_options_must_be_defined(self):
-        # The script is not configured if no environment variable is defined.
-        self.assertFalse(script.is_configured())
+    def test_script_configuration_options_must_be_defined(self):
+        self.assertFalse(script.is_configured())  # No environment variable is yet defined
 
-        # The script is configured if environment variables for script's options are defined.
-        for option in script.REQUIRED_OPTIONS:
+        for option in script.REQUIRED_OPTIONS:  # Set up environment variables
             os.environ[option] = str()
 
         self.assertTrue(script.is_configured())
 
-        # Cleanup environment variables for next unit tests.
-        for option in script.REQUIRED_OPTIONS:
+        for option in script.REQUIRED_OPTIONS:  # Clean up environment variables
             os.environ.pop(option)
 
     def test_find_blu_ray_directories_in_downloaded_files(self):
-        download_path = FIXTURES_DIR / 'downloads/download_1'
+        download_path = FIXTURES_DIR / 'downloads/blu_ray_discs_as_directories'
         discs = [download_path / 'movie' / 'disc_{}'.format(i) for i in range(1, 3)]
 
-        self.assertEqual(script.find_blu_ray_sources(download_path, multi=0), ("file", discs))
-        self.assertEqual(script.find_blu_ray_sources(download_path, multi=1), ("file", discs[0]))
+        for i in range(-1, 1):
+            self.assertEqual(script.find_blu_ray_discs(download_path, disc_set=i), (None, list()))
+        for i in range(1, 4):
+            self.assertEqual(script.find_blu_ray_discs(download_path, disc_set=i), ("file", discs[:i]))
 
     def test_find_blu_ray_iso_images_in_downloaded_files(self):
-        download_path = FIXTURES_DIR / 'downloads/download_2'
+        download_path = FIXTURES_DIR / 'downloads/blu_ray_discs_as_iso_images'
         discs = [download_path / 'movie' / 'disc_{}.iso'.format(i) for i in range(1, 3)]
 
-        self.assertEqual(script.find_blu_ray_sources(download_path, multi=0), ("iso", discs))
-        self.assertEqual(script.find_blu_ray_sources(download_path, multi=1), ("iso", discs[0]))
-        self.assertEqual(script.find_blu_ray_sources(download_path, multi=2), ("iso", discs))
+        for i in range(-1, 1):
+            self.assertEqual(script.find_blu_ray_discs(download_path, disc_set=i), (None, list()))
+        for i in range(1, 4):
+            self.assertEqual(script.find_blu_ray_discs(download_path, disc_set=i), ("iso", discs[:i]))
 
     @unittest.mock.patch('script.bluray_to_mkv.find_makemkv_binary')
     @unittest.mock.patch('script.bluray_to_mkv.subprocess.Popen')
     def test_identify_movie_titles_in_makemkvcon_output(self, mock_popen, mock_find_makemkv):
-        source = {'type': 'iso', 'path': pathlib.PurePath('/downloads/movie.iso')}
+        # Initialization.
+        makemkvcon_binary = PurePath('/usr/bin/makemkvcon')
+        mock_find_makemkv.return_value = makemkvcon_binary
 
-        mock_find_makemkv.return_value = pathlib.PurePath('/usr/bin', script.MAKEMKV_BINARY)
+        disc = {'type': 'iso', 'path': PurePath('/downloads/movie.iso')}
+        titles = [
+            {'chapters': 16, 'duration': timedelta(0, 8074), 'mkv': 'MOVIE_t04.mkv', 'number': 4, 'size': 31.8},
+            {'chapters': 4, 'duration': timedelta(0, 2488), 'mkv': 'MOVIE_t03.mkv', 'number': 3, 'size': 6.0},
+            {'chapters': 2, 'duration': timedelta(0, 1408), 'mkv': 'MOVIE_t02.mkv', 'number': 2, 'size': 3.4},
+            {'chapters': 2, 'duration': timedelta(0, 1080), 'mkv': 'MOVIE_t01.mkv', 'number': 1, 'size': 2.6},
+            {'chapters': 2, 'duration': timedelta(0, 693), 'mkv': 'MOVIE_t00.mkv', 'number': 0, 'size': 1.6},
+        ]
 
-        with (FIXTURES_DIR / 'makemkv' / 'makemkvcon_info_output.txt').open('r', buffering=1) as f:
-            mock_popen.return_value.__enter__.return_value.stdout = f
-            title = script.identify_movie_titles(source)
+        makemkv_command = [makemkvcon_binary, '-r', 'info', '{type}:"{path}"'.format(**disc)]
 
-        mock_find_makemkv.assert_called_once_with()
-        mock_popen.assert_called_once_with(
-            [mock_find_makemkv.return_value, '-r', 'info', '{type}:{path}'.format(**source)],
-            stderr=subprocess.STDOUT, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
-        self.assertEqual(title, {'number': 4, 'fname': 'MOVIE_t04.mkv', 'chapters': 16, 'size': 31.8})
+        # Tests.
+        for i in range(-1, 1):
+            self.assertEqual(script.identify_movie_titles(disc, count=i), list())
+
+        for i in range(1, 6):
+            mock_find_makemkv.reset_mock()
+
+            with (FIXTURES_DIR / 'makemkv' / 'makemkvcon_info_output.txt').open(buffering=1) as f:
+                mock_popen.return_value.__enter__.return_value.stdout = f
+                results = script.identify_movie_titles(disc, count=i)
+
+            self.assertTrue(mock_find_makemkv.called)
+            self.assertEqual(mock_popen.call_args[0][0], makemkv_command)
+            self.assertEqual(results, titles[:i])
 
     @unittest.mock.patch('script.bluray_to_mkv.find_makemkv_binary')
     @unittest.mock.patch('script.bluray_to_mkv.subprocess.Popen')
-    def test_convert_bluray_to_mkv(self, mock_popen, mock_find_makemkv):
+    def test_convert_a_bluray_movie_title_to_mkv(self, mock_popen, mock_find_makemkv):
+        # Initialization.
         movie = "Super Movie"
-        source = {'type': 'iso', 'path': pathlib.PurePath('/downloads/movie.iso')}
-        title = {'number': 4, 'fname': 'MOVIE_t04.mkv', 'chapters': 16, 'size': 31.8}
-        destination = pathlib.PurePath('/library/movies')
-        profile = pathlib.PurePath('/home/user/.MakeMKV/makemkvcon.mmcp.xml')
-        mkv_dst_path = destination / '{}.mkv'.format(movie)
+        disc = {'type': 'iso', 'path': PurePath('/downloads/movie.iso')}
+        title = {'chapters': 16, 'duration': timedelta(0, 8074), 'mkv': 'MOVIE_t04.mkv', 'number': 4, 'size': 31.8}
+        profile = PurePath('/home/user/.MakeMKV/makemkvcon.mmcp.xml')
 
-        mock_find_makemkv.return_value = pathlib.PurePath('/usr/bin', script.MAKEMKV_BINARY)
-        mock_popen.return_value.returncode = 0
-        mock_destination = unittest.mock.MagicMock()
-        mock_destination.__truediv__.return_value.with_name.return_value = mkv_dst_path
+        destination_directory = PurePath('/library/movies')
+        mkv_name = '{0} - {1}.mkv'.format(movie, title['number'])
+        mkv_path = destination_directory / mkv_name
 
-        with (FIXTURES_DIR / 'makemkv' / 'makemkvcon_mkv_output.txt').open('r', buffering=1) as f:
-            mock_popen.return_value.stdout = f
-            mkv_real_path = script.convert_to_mkv(movie, source, title, mock_destination, profile)
+        makemkv_binary = PurePath('/usr/bin/makemkvcon')
+        mock_find_makemkv.return_value = makemkv_binary
 
-        self.assertEqual(mkv_real_path, mkv_dst_path)
-        mock_find_makemkv.assert_called_once_with()
-        mock_popen.assert_called_once_with(
-            [mock_find_makemkv.return_value, '--profile={}'.format(profile),
-             'mkv', '{type}:{path}'.format(**source), title['number'], mock_destination],
-            stderr=subprocess.STDOUT, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
-        mock_destination.__truediv__.assert_called_once_with(title['fname'])
-        mock_destination.__truediv__.return_value.with_name.assert_called_once_with('{}.mkv'.format(movie))
+        mock_destination_directory = unittest.mock.MagicMock()
+        mock_destination_directory.__truediv__.return_value.with_name.return_value = mkv_path
+
+        makemkv_command = [makemkv_binary, '--profile="{}"'.format(profile), 'mkv', '{type}:"{path}"'.format(**disc),
+                           title['number'], mock_destination_directory]
+
+        # Test.
+        with (FIXTURES_DIR / 'makemkv' / 'makemkvcon_mkv_output.txt').open(buffering=1) as f:
+            mock_popen.return_value.__enter__.return_value.stdout = f
+            movie_mkv = script.convert_movie_to_mkv(movie, disc, title, mock_destination_directory, profile)
+
+        # Test assertions.
+        self.assertTrue(mock_find_makemkv.called)
+        self.assertEqual(mock_popen.call_args[0][0], makemkv_command)
+        mock_destination_directory.__truediv__.assert_called_once_with(title['mkv'])
+        mock_destination_directory.__truediv__.return_value.with_name.assert_called_once_with(mkv_name)
+        self.assertEqual(movie_mkv, mkv_path)
 
 
 if __name__ == '__main__':
